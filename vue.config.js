a0baef28f507b5af1299d2e8944e9e840191ea3e
gaygaydesong <1690063074@qq.com>
2020-08-29 16:05:10 +0800

    pages服务器

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
 	// 如果万一需要修改 webpack 配置
 	// 现在的封装又太深了, 可以直接添加 configureWebpack 属性进行修改
-	configureWebpack: {}
+	configureWebpack: {},
+
+	publicPath: process.env.NODE_ENV === 'production' ? '/news-project/' : '/'
 }
